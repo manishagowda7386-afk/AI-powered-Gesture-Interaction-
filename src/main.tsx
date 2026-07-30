f8c42fc30c67ef10b7c9e3f0de6740ad931dab82
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-30 06:32:48 +0000

    Configure application entry point

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+
+import App from "./App";
+import "./index.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster
+          richColors
+          position="top-right"
+          closeButton
+          duration={3000}
+        />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
